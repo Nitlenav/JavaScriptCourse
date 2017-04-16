b907d7852533d1970435eeb854d3ee084c1d1053
Aleksandr <Nitlenav@gmail.com>
2017-04-16 23:55:09 +0300

    Что то сделал, а толку мало.

--- Tasks/tweets_analysis/index.js
+++ Tasks/tweets_analysis/index.js
@@ -1,10 +1,9 @@
 'use strict';
 
 const LineByLineReader = require('line-by-line'),
-    lr = new LineByLineReader('./input/dataSet.csv');
+lr = new LineByLineReader('./input/dataSet.csv');
 
 const data = [];
-
 
 lr.on('error', function (err) {
     // 'err' для вывода ошибок 
@@ -13,33 +12,14 @@ lr.on('error', function (err) {
 });
 
 lr.on('line', function (line) {
-    // pause emitting of lines...
-    //let flag = /(\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)/g;
-    //let  adres = flag.exec(line)
-    let MapAutors = new Map();
-    let autors = line.split(";");
-
- if (autors[4] != undefined){
-     if (MapAutors.get(autors[4]))
-     {
-let countAutors = MapAutors.get(autors[4])
- ++countAutors
-MapAutors.set(autors[4], countAutors)
-     }
-     else
-     {
-MapAutors.set(autors[4],1)
-     }
- }
-
-
+    let countLine = 0;       //при финальной сдаче нужно удалить
+    while (countLine > 500){ //при финальной сдаче нужно удалить
     lr.pause();
-    // ...do your asynchronous line processing..
     setTimeout(function () {
-        // ...and continue emitting lines.
         data.push(line);
         lr.resume();
-    }, 100);
+    }, 10);
+countLine++}                //при финальной сдаче нужно удалить
 });
 
 lr.on('end', function () {
@@ -48,6 +28,74 @@ lr.on('end', function () {
     console.log("End");
 });
 
-function main () {
-
+function main () {}
+function asinc (){
+    for(let word in data)
+{
+    console.log(word)
 }
+}
+/*
+function mass(dataMas){
+return new Promise((resolve, reject)=>{
+    if (dataMas == undefined){
+        reject(new Error('Данные в масиве отсутствубют'))
+    }
+else{
+let mapTweet = new Map();
+for (let lines in dataMas) {
+console.log(lines)
+resolve(lines)
+}
+}
+resolve(lines)
+})
+}
+mass(data)
+.then((result) => console.log(result))
+.catch((error) => console.log(error)); 
+*/
+
+
+
+/*
+
+function tweet( function (masline) { // 10 наиболее часто встречающихся слов в Tweet
+    let MaTweet = new Map();
+    let tweet = line.split(";");
+        console.log(line)
+        if (tweet[7] !== undefined){
+    let masTweet = tweet[7].split(" ");
+    console.log(line)
+for(let word in masTweet){
+
+}}
+    lr.pause();
+    // ...do your asynchronous line processing..
+    setTimeout(function () {
+        // ...and continue emitting lines.
+        data.push(line);
+        lr.resume();
+    }, 10);
+}, );
+
+
+/**
+     let MapAutors = new Map();
+    let autors = line.split(";");
+ if (autors[4] != undefined){
+
+     let autor = autors[4]
+     if (MapAutors.get(autor) !== undefined)
+     {
+let countAutors = MapAutors.get(autor)
+ ++countAutors
+MapAutors.set(autor, countAutors)
+     }
+     else
+     {
+MapAutors.set(autor,1)
+console.log(MapAutors)
+     }
+ }
+ */
