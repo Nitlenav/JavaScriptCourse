2f4f90443e2c1096466dd54bb4435a25290902aa
Aleksandr <Nitlenav@gmail.com>
2017-04-24 01:37:07 +0300

    По моему программа закончена.

--- Tasks/tweets_analysis/index.js
+++ Tasks/tweets_analysis/index.js
@@ -1,108 +1,124 @@
 'use strict';
 
 const LineByLineReader = require('line-by-line'),
-lr = new LineByLineReader('./input/dataSet.csv');
-
-    function Place(country, city) {
-        this.country = country;
-        this.city = city;
-        this.placeIndex = 0;
-    };
-
-    function PopularUser(nickName, followers) {
-        this.nickName = nickName;
-        this.followers = followers;
-    };
-
-    function PopularTweet(nickName, tweetContent, rts) {
-        this.nickName = nickName;
-        this.tweetContent = tweetContent;
-        this.retweets = rts;
-    };
+    lr = new LineByLineReader('./input/dataSet.csv');
 
 const data = [];
-const citysMap = new Map();
-const authorsMap = new Map();
-const tweetsMap = new Map();
+
+// 2. Создаём карты с переменными?для дальнейшего анализа
+let popularAutors = new Map();
+let popularTweet = new Map();
+let popularCity = new Map();
 
 function main () {}
 
 lr.on('error', function (err) {
-    // 'err' для вывода ошибок 
-    console.log(err);
-    lr.close();
+    new Error("Кака та Oшибка");
 });
 
 lr.on('line', function (line) {
+        //console.log(line)
+        data.push(line);
+        parseTweet(data);
 
-lr.pause();
-        data.push(line.split(";"));
-        for(let i = 2; i< data.length; i++){
-            let proxyLine = data[i];
-            if(proxyLine[4] != undefined && proxyLine[14] != undefined && !authorsMap.has(proxyLine[4])) {
-                let author = new PopularUser(proxyLine[4], proxyLine[14] );
-                authorsMap.set(proxyLine[4], author);
-            };
+    lr.pause();
+    setTimeout(function () {
+        lr.resume();
+    }, 10);
 
-            if(!tweetsMap.has(proxyLine[6])){
-                let tweet = new PopularTweet(proxyLine[4], proxyLine[6], proxyLine[8]);
-                tweetsMap.set(proxyLine[6], tweet);
-            };
+setTimeout(function () {
+analisDataAutors(popularAutors);
+analisDataTweet(popularTweet);
+analisDataTCity(popularCity);
+}, 6000)
 
-            if(citysMap.has(proxyLine[12])){
-                citysMap.get(proxyLine[12]).placeIndex = citysMap.get(proxyLine[12]).placeIndex +1;
-            }else if(proxyLine[11] != undefined && proxyLine[12] != undefined && proxyLine[12] != ";" && proxyLine[12] !="" && proxyLine[11] !="" && proxyLine[11] !=";"){
-                let place = new Place(proxyLine[11], proxyLine[12]);
-                citysMap.set(proxyLine[12], place)
-            }
-        }
-
-        setTimeout(function () {
-                                lr.resume();
-        }, 100);
-
-        setTimeout(function () {
-                                analytics(authorsMap, tweetsMap)
-        }, 5000)
-    });
-
+});
+ 
 lr.on('end', function () {
     main();
-    console.log("End");
+    console.log("Вот и конец")
 });
 
-function analytics(authorsCollections, tweetsCollection) {
+function parseTweet (dataTweet){
+    return new Promise(function(resolve, reject) {
+// Берём последнюю запись в масиве, передаём в переменную и удаляем её.
 
-        let maxU = 0;
-        let maxyMan;
+let parseLine = data.pop();
+let masLine = parseLine.split(";");
 
-        for(let user of authorsCollections.values()) {
-            if(user.followers*1 > maxU*1){
-                maxyMan = user;
-                maxU = maxyMan.followers;
+            //собираем статистику по самым популярным авторам; самый популярный тот, у кого фолловеров больше
+            if(+masLine[14] != NaN && masLine[4] != '' && masLine[4] != undefined && masLine[14] != undefined && !popularAutors.has(masLine[4])) {
+                popularAutors.set(masLine[4], +masLine[14]);
+                //console.log(popularAutors);
+            } else if (popularAutors.has(masLine[4])){
+                let countFollowers = popularAutors.get(masLine[4]) +  (+masLine[14]) ;
+                popularAutors.set(masLine[4], countFollowers);
+            };
+
+            //собираем статистику по самым популярным твитам; самый популярный тот, у которого ретвитов больше;
+            if(masLine[4] != undefined && masLine[8] != undefined &&  masLine[4] != '' && masLine[8] != '' && !popularTweet.has(masLine[4])){
+                popularTweet.set(masLine[4], +masLine[8]);
+                //console.log(popularTweet);
+            } else if (popularTweet.has(masLine[4])){
+                let countRetweet = popularTweet.get(masLine[4]) + (+masLine[8]);
+                popularTweet.set(masLine[4], countRetweet);
+            };
+
+            //собираем статистику по самым активным городам; количество твитов, сделанных в одном городе
+           if(masLine[11] != undefined && masLine[12] != undefined && masLine[12] != ";" && masLine[12] !="" && masLine[11] !="" && masLine[11] !=";" && !popularCity.has(masLine[12]))
+           {
+                popularCity.set(masLine[12],  1);
+                //console.log(popularCity);
+            } else if(popularCity.has(masLine[12])){
+                let countTweet = popularCity.get(masLine[12]) + 1;
+                popularCity.set(masLine[12], countTweet);
             }
-        };
 
-        let maxT = 0;
-        let maxyTweet;
-        for(let tweet of tweetsCollection.values()) {
-            if(tweet.retweets*1 >maxT*1){
-                maxyTweet = tweet;
-                maxT = maxyTweet.retweets;
-            }
-        };
+    function errors () {
+      reject(new Error("Кака та ошибка"));
+    };      
 
-        let maxI = 0;
-        let maxyCity;
-        for(let city of citysMap.values()) {
-            if(city.placeIndex*1 >maxI*1){
-                maxI = city.placeIndex*1;
-                maxyCity = city;
-            }
-        };
+});}
 
-        console.log("Один из популярных пользователей Твиттера - " + maxyMan.nickName + "; за ним следят " + maxyMan.followers + " пользователей.");
-        console.log("Самый популярный Твит - " + maxyTweet.tweetContent + "; его перепостили " + maxyTweet.retweets + " раз.");
-        console.log("Самый твитящий город - " + maxyCity.city + " из " + maxyCity.country +"; в 'этом городе твитят " + maxyCity.placeIndex +" пользователей");
-        console.log();
-};
+function analisDataAutors (valAutors){
+        let maxVal = 0;
+        let keyMapAutors;
+valAutors.forEach(function(value, key, valAutors) {
+    if (value > maxVal){
+        maxVal = value;
+        keyMapAutors = key
+  //console.log(key + " = " + value);
+    }
+
+})
+console.log("Самый популярный пользователь Твиттера - " + keyMapAutors + "; за ним следят " + maxVal + " пользователей.");
+}
+
+function analisDataTweet (valTweet){
+        let maxTwet = 0;
+        let keyMapTwet;
+valTweet.forEach(function(value, key, valTweet) {
+    if (value > maxTwet){
+        maxTwet = value;
+        keyMapTwet = key
+  //console.log(key + " = " + value);
+    }
+
+})
+console.log("Самый популярный Твит - " + keyMapTwet + "; его перепостили " + maxTwet + " раз.");
+}
+
+function analisDataTCity (valCity){//popularCity
+        let maxCity = 0;
+        let keyMapCity;
+valCity.forEach(function(value, key, valCity) {
+    if (value > maxCity){
+        maxCity = value;
+        keyMapCity = key
+  //console.log(key + " = " + value);
+    }
+
+})
+console.log("Самый твитящий город - " + keyMapCity  + " в этом городе твитят " + maxCity +" пользователей");
+console.log();
+}
